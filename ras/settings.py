058daf29c7602dbd4aa3e7dd0b971e38286f1377
edson1449 <edsonvega144@gmail.com>
2025-12-10 21:06:20 -0500

    init commit

--- ras/settings.py
+++ ras/settings.py
@@ -99,12 +99,11 @@ WSGI_APPLICATION = 'ras.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'railway',
-        'USER': 'root',
-        'PASSWORD': 'zozKbyYKpOPtbZsgpEeMKCXglLpDtTON',
-        'HOST': 'mysql.railway.internal',
-        'PORT': '3306'
-
+        'NAME': os.environ.get('railway'),
+        'USER': os.environ.get('root'),
+        'PASSWORD': os.environ.get('zozKbyYKpOPtbZsgpEeMKCXglLpDtTON'),
+        'HOST': os.environ.get('mysql.railway.internal'),
+        'PORT': os.environ.get('3306'),
     }
 }
 
